0d5f92121c348ac2a0acd0bf4c9a913d20e34329
zqingfa <344266686@qq.com>
2018-10-31 10:04:40 +0800

    Update gettoken.sh
    取消显示“token”

--- 004.greatwall-console/gettoken.sh
+++ 004.greatwall-console/gettoken.sh
@@ -2,7 +2,7 @@
 tokenName=`kubectl get secrets -n kube-system  | awk /greatwall-console/{'print $1'}`
 
 #token=`kubectl -n kube-system  get $tokenName -o jsonpath={.data.token}| base64 -d`
-token=`kubectl describe -n kube-system secrets $tokenName | grep token | grep -v Name | grep -v Type`
+token=`kubectl describe -n kube-system secrets $tokenName | grep token | grep -v Name | grep -v Type | awk {'print $2'}`
 echo "================================================================================"
 echo ""
 echo "Please copy text for login greatwall console"
